f35c0aba18fa87e1910e7ef6708aa1a5f5236f28
Piotr Krysik <ptrkrysik@gmail.com>
2022-08-03 10:37:17 +0200

    mpm: x411: added driver for LMX2594

--- mpm/python/usrp_mpm/chips/__init__.py
+++ mpm/python/usrp_mpm/chips/__init__.py
@@ -13,4 +13,5 @@ from .lmk04828 import LMK04828
 from .lmk04832 import LMK04832
 from .lmk03328 import LMK03328
 from .lmk05318 import LMK05318
+from .lmx2594 import LMX2594
 from . import ic_reg_maps
